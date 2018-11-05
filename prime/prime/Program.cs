255b8347eaced961e64808db910a1653c14778bc
Kermottos <kermottos69@gmail.com>
2018-11-05 17:03:58 +0100

    First commit

---
+++ prime/prime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Liczby pierwsze pomiędzy 0 i 100 ---");
+
+            for (int i = 0; i < 100; i++)
+            {
+                bool prime = PrimeTool.IsPrime(i);
+
+                if (prime)
+                {
+                    Console.Write("Pierwsze: ");
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
